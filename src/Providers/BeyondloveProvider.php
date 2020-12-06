1de0ba325b4dff22b26ca56726cf167624e7a769
Mark Dingemanse <dingemansemark@gmail.com>
2020-12-06 13:29:59 +0100

    route

--- src/Providers/BeyondloveProvider.php
+++ src/Providers/BeyondloveProvider.php
@@ -23,30 +23,11 @@ class BeyondloveProvider extends ServiceProvider
      */
     public function boot()
     {
-        parent::boot();
-    }
-
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
-    {
-        $this->routes();
-
-    }
-
-    /**
-     * Define the beyondlove routes
-     *
-     * @return void
-     */
-    protected function routes()
-    {
         Route::get('/', 'HomepageController@launch')->middleware('web')->namespace($this->namespace)->name('homepage');
         Route::get('random', 'HomepageController@getRandomFile')->namespace($this->namespace)->name('getFile');
         Route::get('upload','HomepageController@uploadfileview')->namespace($this->namespace)->name('upload_background_view');
         Route::post('upload','HomepageController@uploadFile')->namespace($this->namespace)->name('upload_background_post');
+    
+        parent::boot();
     }
 }
