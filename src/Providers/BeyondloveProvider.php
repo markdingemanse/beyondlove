556320a8eb6bcfbda913a9f10bfb0f0e38b05f19
Mark Dingemanse <dingemansemark@gmail.com>
2021-08-28 15:53:00 +0200

    added empty provider

---
+++ src/Providers/BeyondloveProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MarkDingemanse\Beyondlove\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BeyondloveProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
